1368c2ccfa20614f117efb43eb500e6b6f4f74b4
skoulas <fotis_skoulas@hotmail.com>
2022-01-04 20:58:52 +0200

    added coffe-store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import coffeeStoresData from "../../data/coffee-stores.json";
+import Head from "next/head";
 
 export function getStaticProps(staticProps) {
   const params = staticProps.params;
@@ -14,8 +15,15 @@ export function getStaticProps(staticProps) {
 }
 
 export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
   return {
-    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    paths,
     fallback: true,
   };
 }
@@ -26,18 +34,19 @@ const CoffeeStore = (props) => {
   if (router.isFallback) {
     return <div>Loading... </div>;
   }
+  const { address, name, neighbourhood } = props.coffeeStore;
 
   return (
     <div>
-      Coffee Store Page {router.query.id}
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">
         <a>Back to home </a>
       </Link>
-      <Link href="/coffee-store/dynamic">
-        <a>Go to page dynamic</a>
-      </Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   );
 };
