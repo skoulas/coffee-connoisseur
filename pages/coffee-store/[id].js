4a5b18e01c1234fee54bbc34483aabb3c29e8d60
skoulas <fotis_skoulas@hotmail.com>
2022-01-09 10:57:27 +0200

    added styling coffe-store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 import cls from "classnames";
 
-import styles from "../../styles/coffee-store.module.css";
-
 import coffeeStoresData from "../../data/coffee-stores.json";
+
+import styles from "../../styles/coffee-store.module.css";
 
 export function getStaticProps(staticProps) {
   const params = staticProps.params;
@@ -28,6 +28,7 @@ export function getStaticPaths() {
       },
     };
   });
+
   return {
     paths,
     fallback: true,
@@ -38,12 +39,13 @@ const CoffeeStore = (props) => {
   const router = useRouter();
 
   if (router.isFallback) {
-    return <div>Loading... </div>;
+    return <div>Loading...</div>;
   }
+
   const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
 
   const handleUpvoteButton = () => {
-    console.log("handle  upvote");
+    console.log("handle updvote!");
   };
 
   return (
@@ -51,16 +53,15 @@ const CoffeeStore = (props) => {
       <Head>
         <title>{name}</title>
       </Head>
-
       <div className={styles.container}>
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
             <Link href="/">
-              <a>Back to home </a>
+              <a>Back to home</a>
             </Link>
           </div>
           <div className={styles.nameWrapper}>
-            <h1 className={styles.name}> {name}</h1>
+            <h1 className={styles.name}>{name}</h1>
           </div>
           <Image
             src={imgUrl}
@@ -70,44 +71,22 @@ const CoffeeStore = (props) => {
             alt={name}
           ></Image>
         </div>
-
         <div className={cls("glass", styles.col2)}>
           <div className={styles.iconWrapper}>
-            <Image
-              src="/static/icons/places.svg"
-              width="24"
-              height="24"
-              alt="places icon"
-            />
+            <Image src="/static/icons/places.svg" width="24" height="24" />
             <p className={styles.text}>{address}</p>
-
-            <div className={styles.iconWrapper}>
-              <Image
-                src="/static/icons/nearMe.svg"
-                width="24"
-                height="24"
-                alt="places icon"
-              />
-              <p className={styles.text}>{neighbourhood}</p>
-
-              <div className={styles.iconWrapper}>
-                <Image
-                  src="/static/icons/star.svg"
-                  width="24"
-                  height="24"
-                  alt="places icon"
-                />
-                <p className={styles.text}>1</p>
-              </div>
-
-              <button
-                className={styles.upVoteButton}
-                onClick={handleUpvoteButton}
-              >
-                Up Vote !
-              </button>
-            </div>
           </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width="24" height="24" />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
         </div>
       </div>
     </div>
