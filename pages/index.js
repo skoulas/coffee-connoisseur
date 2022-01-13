4ff6c2a8ef283b59882eff3cb03a45185ffdfceb
skoulas <fotis_skoulas@hotmail.com>
2022-01-13 20:20:21 +0200

    display coffe stores by location on the page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { fetchCoffeeStores } from "../lib/coffee-store";
 import useTrackLocation from "../hooks/use-track-location";
 import { useEffect } from "react";
+import { useState } from "react";
 
 import styles from "../styles/Home.module.css";
 
@@ -24,6 +25,9 @@ export default function Home(props) {
   const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
     useTrackLocation();
 
+  const [coffeeStores, setCoffeeStores] = useState("");
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null);
+
   console.log({ latLong, locationErrorMsg });
 
   useEffect(async () => {
@@ -31,9 +35,11 @@ export default function Home(props) {
       try {
         const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
         console.log({ fetchedCoffeeStores });
+        setCoffeeStores(fetchedCoffeeStores);
       } catch (error) {
         //error
-        console.log(error);
+        console.log({ error });
+        setCoffeeStoresError(error.message);
       }
     }
   }, [latLong]);
@@ -55,10 +61,34 @@ export default function Home(props) {
           buttonText={isFindingLocation ? "Locating ..." : "View stores nearby"}
           handleOnClick={handleOnBannerBtnClick}
         />
+
         {locationErrorMsg && <p>Something went wrong :{locationErrorMsg}</p>}
+        {coffeeStoresError && <p>Something went wrong :{coffeeStoresError}</p>}
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+
+        {coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores near me</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
+
         {props.coffeeStores.length > 0 && (
           <div className={styles.sectionWrapper}>
             <h2 className={styles.heading2}>Zwgrafou stores</h2>
